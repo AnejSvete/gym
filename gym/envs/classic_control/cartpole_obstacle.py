354df4ef078cef9610fe9211572b67e060e2b0c0
Anej Svete <anej.svete@protonmail.com>
2020-07-05 16:57:23 +0200

    Obstacle :).

--- gym/envs/classic_control/cartpole_obstacle.py
+++ gym/envs/classic_control/cartpole_obstacle.py
@@ -229,14 +229,14 @@ class CartPoleObstacleEnv(CartPoleExtensionEnv):
 
         elif self.mode in ['test', 'eval']:
 
-            return self.times_at_goal >= self.evaluation_stable_duration or \
-                (not failed and
-                 in_goal_state and
-                 self.episode_step >= self.max_episode_steps - 1)
+            # return self.times_at_goal >= self.evaluation_stable_duration or \
+            #     (not failed and
+            #      in_goal_state and
+            #      self.episode_step >= self.max_episode_steps - 1)
 
-            # return not failed and \
-            #     in_goal_state and \
-            #     self.episode_step >= self.max_episode_steps - 1
+            return not failed and \
+                in_goal_state and \
+                self.episode_step >= self.max_episode_steps - 1
 
     def has_failed(self, x, theta):
         return not self.x_min <= x <= self.x_max or \
